f733243802cc506892434f091e9cfd1ea998b5ec
alanmalan <110788788+alanmalan@users.noreply.github.com>
2023-05-24 23:15:52 -0300

    Add files via upload

---
+++ T.c
@@ -0,0 +1,83 @@
+#include <stdio.h> 
+#include <conio.h>
+#include <windows.h>
+
+void egg(char* somethingrelevant){
+	fgets(somethingrelevant, 261, stdin);
+}
+
+int main(int argc, char** argv){
+	FILE * file;
+	file = fopen (argv[1], "r");
+	int code_lenght = 128;
+	char code[code_lenght];
+	int tape_length = 16;
+	unsigned char tape[tape_length];
+	int pointer = 0;
+	int index = 0;
+	int cycle = 0;
+	int imp;
+	int STR_MEM_LOC;
+	unsigned char temp_mem;
+	egg(code);
+	while (pointer < tape_length){
+		tape[pointer] = 0;
+		pointer++;
+	} pointer = 0;
+	while (1){
+		if (index >= code_lenght){
+			return 0;
+		} else {
+			index++;
+		}
+		if (cycle > 10){
+			cycle = 0;
+		}
+		switch(code[index]){
+				case ('o'):
+					cycle++;
+					break;
+				case ('r'):
+						switch (cycle){
+							case 0:
+								return 0;							//Zero, ends program
+								break;
+							case 1:
+								printf("%d \n", temp_mem);		//equivalent to BF "."
+								break;
+							case 2:
+								pointer++;							//equivalent to > in BF
+								break;
+							case 3:
+								pointer--;							//equivalent to < in BF
+								break;
+							case 4:
+								imp=getchar();
+								tape[pointer] = (int)imp;			//equivalent to "," in BF
+								break;
+							case 5:
+								tape[pointer]++;
+								break;
+							case 6:
+								tape[pointer]--;
+								break;
+							case 7:
+								STR_MEM_LOC = pointer;
+								break;
+							case 8:
+								if (tape[pointer] != 0){
+									pointer = STR_MEM_LOC;
+								} 
+								break;
+							case 9:
+								temp_mem = tape[pointer];
+								break;
+							case 10:
+								tape[pointer] = temp_mem;
+								break;
+						}
+					cycle++;
+					break;
+			} 
+		}
+}
